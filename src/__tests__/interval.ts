1e79ef08152ff3d35ac836b269071655dd1d9c5d
Ray Deck <ray@raydeck.com>
2021-03-29 07:53:44 -0400

    test: add more test coverage (9%!)

--- src/__tests__/interval.ts
+++ src/__tests__/interval.ts
@@ -1,5 +1,13 @@
 import { validateSchedule, isValidTimeZone } from "../";
 describe("isValidTimeZone", () => {
+  // it("Check missing intl", () => {
+  // const i = Intl;
+  // //@eslint-disable-line @ts-ignore-line  @eslint-disable-next-line
+  // global.Intl = undefined;
+  // expect(() => isValidTimeZone("America/New_York")).toThrow();
+  // // @eslint-disable-line @ts-ignore @eslint-disable-next-line
+  // global.Intl = i;
+  // });
   it("America/New_York should succeed", () =>
     expect(isValidTimeZone("America/New_York")).toBe(true));
   it("America/New_Amsterdam should fail", () =>
@@ -8,5 +16,23 @@ describe("isValidTimeZone", () => {
 describe("Validator", () => {
   it("isDefined", () => {
     expect(validateSchedule).toBeDefined();
+  });
+  it("blankHours", () => {
+    expect(() =>
+      validateSchedule({ hours: [], timezone: "Bibble Bob" })
+    ).toThrow();
+  });
+  it("badHours", () => {
+    expect(() =>
+      validateSchedule({ hours: [-1], timezone: "Bibble Bob" })
+    ).toThrow();
+    expect(() =>
+      validateSchedule({ hours: [1.5], timezone: "Bibble Bob" })
+    ).toThrow();
+  });
+  it("badTZ", () => {
+    expect(() =>
+      validateSchedule({ hours: [8], timezone: "Bibble Bob" })
+    ).toThrow();
   });
 });
